8838744b59042fa7e36379908b103bae24c7de3b
azhar_azad <azhar.u.azad@gmail.com>
2023-01-03 03:04:34 +0600

    builder pattern done

---
+++ src/examples/creational/builder/HairColor.java
@@ -0,0 +1,18 @@
+package examples.creational.builder;
+
+/***
+ * HairColor enumeration.
+ */
+public enum HairColor {
+
+    WHITE,
+    BLOND,
+    RED,
+    BROWN,
+    BLACK;
+
+    @Override
+    public String toString() {
+        return name().toLowerCase();
+    }
+}
---
+++ src/examples/creational/builder/Weapon.java
@@ -0,0 +1,18 @@
+package examples.creational.builder;
+
+/***
+ * Weapon enumeration.
+ */
+public enum Weapon {
+
+    DAGGER,
+    SWORD,
+    AXE,
+    WAR_HAMMER,
+    BOW;
+
+    @Override
+    public String toString() {
+        return name().toLowerCase();
+    }
+}
